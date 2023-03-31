14c27f7e19c4d1e3eb719ef9a06dfb5cc5baaa19
Arlak <arlak.a@orcsoft.co.th>
2023-03-31 16:44:51 +0700

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Arlak Abdulloh on 31/3/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
